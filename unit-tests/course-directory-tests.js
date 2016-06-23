8451562876250dff9d5c2eaa91f7d6fa2f5065b9
jenbennet <jenny.castro@gmail.com>
2016-06-22 20:28:44 -0600

    New files for the course catalogue

---
+++ unit-tests/course-directory-tests.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect; // Using "expect" style of Chai.
+var CourseDirectory = require('./../src/course-directory');
+
+describe('CourseDirectory', function() {
+    
+    describe('Constructor', function() {
+        
+        describe('Fail path', function(){
+            
+        });
+        
+        describe('Pass path', function(){
+            
+        });
+    });
+});
