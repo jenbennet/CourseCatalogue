fd386e4663b80e3a5063544023ff3a6471c4daba
jenbennet <jenny.castro@gmail.com>
2016-06-23 12:39:18 -0600

    Added utilities and unit tests for parsing the input array

--- unit-tests/course-directory-tests.js
+++ unit-tests/course-directory-tests.js
@@ -6,7 +6,9 @@ describe('CourseDirectory', function() {
     describe('Constructor', function() {
         
         describe('Fail path', function(){
-            
+            it('Should fail to create a new CourseDirectory without an inputArray', function() {
+                expect (function(){ new CourseDirectory(); }).to.throw( TypeError );
+            });
         });
         
         describe('Pass path', function(){
@@ -24,6 +26,84 @@ describe('CourseDirectory', function() {
             it('Should be possible to access CourseDirectory.courseHash', function() {
                 expect(directory.courseHash).not.to.be.undefined;    
             });
+        });
+    });
+    
+    describe('Directory Utility Functions', function() {
+        var directory = new CourseDirectory([]);
+        
+        describe('Fail path', function(){
+            
+            it('Should fail to add empty item to CourseDirectory.courseHash', function() {
+                expect(function() { directory.addItem() }).to.throw(TypeError);
+            });
+            
+            it('Should fail if CourseDirectory.hasPrerequisite() parameter is not an array', function() {
+                expect(function() { directory.hasPrerequisite() }).to.throw(TypeError);
+            });
+        });
+        
+        describe('Pass path', function(){
+            
+            it('Should add items to CourseDirectory.courseHash', function() {
+                expect(function() { directory.addItem('Introduction to Fire', ['Advanced Pyrotechnics']) }).to.not.throw(Error);
+            });
+            
+            it('Should not have a prerequisite', function() {
+                var inputArray = ['Introduction to Fire', ''];
+                
+                expect(directory.hasPrerequisite(inputArray)).to.be.false;
+            });
+            
+            it('Should have a prerequisite', function() {
+                var inputArray = ['Advanced Pyrotechnics', 'Introduction to Fire'];
+                
+                expect(directory.hasPrerequisite(inputArray)).to.be.true;
+            });
+            
+            it('Should return the Course from an \'Course: Prerequisite\' item', function(){
+                var inputArray = ['Introduction to Fire', ''];
+                
+                expect(directory.getCourse(inputArray)).to.be.equal('Introduction to Fire');
+            });
+            
+            it('Should return the Course from an \'Course: \'  item', function(){
+                var inputArray = ['Advanced Pyrotechnics', 'Introduction to Fire'];
+                
+                expect(directory.getCourse(inputArray)).to.be.equal('Advanced Pyrotechnics');
+            });
+            
+            it('Should return the Prerequisite from an \'Course: Prerequisite\'  item', function(){
+                var inputArray = ['Advanced Pyrotechnics', 'Introduction to Fire'];
+                
+                expect(directory.getPrerequisite(inputArray)).to.be.equal('Introduction to Fire');
+            });
+            
+        });
+    });
+    
+    describe('Directory Parsing Functions', function() {
+        
+        describe('Fail path', function(){
+            
+        });
+        
+        describe('Pass path', function(){
+            
+            it('Should read the courseArray and parse the Prerequisite into CourseDirectory.courseHash', function() {
+                var inputArray = ['Introduction to Fire: '];
+                var directory = new CourseDirectory(inputArray);
+                
+                var hash = new Object();
+                hash['Introduction to Fire'] = [];
+
+                
+                expect(function() {
+                    directory.createCourseHash();
+                    //console.log('test ' + directory.courseHash['Introduction to Fire'].pop());
+                }).to.not.throw(Error);
+            });
+            
         });
     });
 });
